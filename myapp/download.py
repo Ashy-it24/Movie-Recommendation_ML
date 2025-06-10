c47c3323b4afa3e5960fe2c8bb50581e154b4a02
Ashy-it24 <aswanthb.it2024@citchennai.net>
2025-06-10 18:42:28 +0530

    Delete myapp directory

--- myapp/download.py
+++
@@ -1,8 +0,0 @@
-import gdown
-import gdown  # Import the library
-
-# Download file from Google Drive
-gdown.download(
-    "https://drive.google.com/uc?id=1AJzMFKZSkwOAuxGpAu8bHEgsq2qCW3aE",  # Google Drive shareable link
-    "data.csv"  # Output filename
-)
